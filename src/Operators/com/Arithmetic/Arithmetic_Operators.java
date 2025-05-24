bf4e672bde9f1ea1cdeeeb666cd4665e599c4bf8
Kunal Chavan <kunal24chavan@gmail.com>
2025-05-24 19:21:08 +0530

    JIRA-1234: updated project structure

---
+++ src/Operators/com/Arithmetic/Arithmetic_Operators.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package Operators.com.Arithmetic;
+
+/**
+ * @author Kunal
+ * This change has been done by Kunal
+ * This code is to show how arithmetic operators are used in Java. Updated Description
+ */
+public class Arithmetic_Operators
+{
+	public static void main(String... args)
+	{
+		int a, b = 10, c = 5, d=10, e =15, g = 20;
+		a = b + c;
+		System.out.println("Value of 'a' after '+' Arithmetic operation is " + a);
+		a = b - c;
+		System.out.println("Value of 'a' after '-' Arithmetic operation is " + a);
+		a = b * c;
+		System.out.println("Value of 'a' after '*' Arithmetic operation is " + a);
+		a = b / c;
+		System.out.println("Value of 'a' after '/' Arithmetic operation is " + a);
+		a = b % c;
+		// this is test comment
+		System.out.println("Value of 'a' after '%' Arithmetic operation is " + a);
+		System.out.println("Value of 'b' after '++' Arithmetic operation is " + b++);
+		System.out.println("Value of 'c' after '--' Arithmetic operation is " + c--);
+	}
+}
