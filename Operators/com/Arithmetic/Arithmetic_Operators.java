4e0850920f768026f6e6f51ebfbbc1c59ecb13c0
Kunal Chavan <kunal24chavan@gmail.com>
2025-05-24 18:59:53 +0530

    JIRA-1234: added comment

--- Operators/com/Arithmetic/Arithmetic_Operators.java
+++ Operators/com/Arithmetic/Arithmetic_Operators.java
@@ -22,6 +22,7 @@ public class Arithmetic_Operators
 		a = b / c;
 		System.out.println("Value of 'a' after '/' Arithmetic operation is " + a);
 		a = b % c;
+		// this is test comment
 		System.out.println("Value of 'a' after '%' Arithmetic operation is " + a);
 		System.out.println("Value of 'b' after '++' Arithmetic operation is " + b++);
 		System.out.println("Value of 'c' after '--' Arithmetic operation is " + c--);
