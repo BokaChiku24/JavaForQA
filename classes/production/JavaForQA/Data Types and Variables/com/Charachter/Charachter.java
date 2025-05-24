d99947c088e7812792b9583085ff09c97932d18e
Kunal Chavan <kunal24chavan@gmail.com>
2025-05-24 23:00:55 +0530

    JIRA-1234: updated project structure

---
+++ classes/production/JavaForQA/Data Types and Variables/com/Charachter/Charachter.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+/**
+ * @author Kunal
+ */
+public class Charachter
+{
+	public static void main(String[] args)
+	{
+		// Use the char keyword to declare character variable
+		char a;
+
+		// Initialize the char variable with value 'K'
+		a = 'K';
+
+		// Print the value of char variable
+		System.out.println("Charachter is: " + a);
+
+		char c;
+
+		c = 'd';
+	}
+}
